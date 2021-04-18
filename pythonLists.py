29fcccbdff84c7460afa4c9331a295e76b80f3e8
Rafael <silva.reis.rafael@gmail.com>
2021-04-18 20:41:59 -0300

    python

---
+++ pythonLists.py
@@ -0,0 +1,21 @@
+fam = ["liz","rafael","123"]
+
+print(fam)
+
+#subseting lists
+print(fam[0])
+
+#list slicing
+fam2 = ["liz",1.78,"emma",1.68]
+
+print(fam2[0:2])
+
+x = [["a", "b", "c"],
+     ["d", "e", "f"],
+     ["g", "h", "i"]]
+print(x[2][0])
+print(x[2][:2])
+print(x[-1][1])
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+#print(areas[-1][1])
+
