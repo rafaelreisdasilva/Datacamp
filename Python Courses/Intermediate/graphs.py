995fde84047a79a797d52a94a33241fd6c217130
Rafael <silva.reis.rafael@gmail.com>
2021-04-26 08:55:49 -0300

    Update graphs.py

--- Python Courses/Intermediate/graphs.py
+++ Python Courses/Intermediate/graphs.py
@@ -2,5 +2,10 @@ import matplotlib.pyplot as plt
 
 year = [1950,1970,1990,2010]
 pop = [2.519,3.692,5.263,6.972]
+#line graph
 plt.plot(year,pop)
+plt.show()
+
+#scatter plot
+plt.scatter(year,pop)
 plt.show()
